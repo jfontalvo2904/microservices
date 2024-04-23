bebe91a5d8b9133d94f327a91c8e3236b950ecd2
= <fontalvo0218@gmail.com>
2024-04-23 13:19:14 -0500

    no se ha implementado eureka

---
+++ discovery-server/src/test/java/com/unimag/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unimag.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
